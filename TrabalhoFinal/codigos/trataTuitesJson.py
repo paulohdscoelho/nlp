a86b8cefddf70358a58cc5a31c2b6c578c7a5b1d
Paulo Coelho <controlpaulo@ufmg.br>
2017-11-08 12:24:17 -0200

    Atualização do Arquivo, corrigindo loop de json

--- TrabalhoFinal/codigos/trataTuitesJson.py
+++ TrabalhoFinal/codigos/trataTuitesJson.py
@@ -6,23 +6,20 @@ from datetime import datetime
 
 #sed -f <(sed 's/.*/s|\\\<&\\\>||g/' stopwords.txt) tweetsCatalunhaPassados.txt > tweetsCatalunha.txt
 
-
 with open('scraperCatalunha.json','r') as textEntrada:
     json_data = textEntrada.readline()
     
 json_data = json.loads(json_data)
 
-with open('tweetsCatalunha.txt') as entrada:
-	lines = [line.split('\t') for line in entrada]
-	for rawTweet in json_data:
-		text = rawTweet.text.strip().replace('\n',' ').replace('\r',' ')
-		with open('tweetsCatalunhaComStW.txt','a') as saida:
-			if(' RT @' not in text):				
-				if 'http' in text:
-					text = text.split('http')[0]				
-				if '.twitter.com' in text:
-					text = text.split('.twitter.com')[0]				
-				text = re.sub(r"(?:\#|\@|http[_,s]?\://)\S+", "", text)
-				text = text.replace('pic','').replace('Twitter','')
-				print(str(text))
-				saida.write(str(text)+'\n')
+for rawTweet in json_data:
+	text = rawTweet.text.strip().replace('\n',' ').replace('\r',' ')
+	with open('tweetsCatalunhaComStW.txt','a') as saida:
+		if(' RT @' not in text):				
+			if 'http' in text:
+				text = text.split('http')[0]				
+			if '.twitter.com' in text:
+				text = text.split('.twitter.com')[0]				
+			text = re.sub(r"(?:\#|\@|http[_,s]?\://)\S+", "", text)
+			text = text.replace('pic','').replace('Twitter','')
+			print(str(text))
+			saida.write(str(text)+'\n')
