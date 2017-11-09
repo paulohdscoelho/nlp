7c82adfe9baa4cc11e2476eb697a475de32e14a4
Paulo Coelho <paulohdscoelho@gmail.com>
2017-11-09 21:07:41 -0200

    Adicionando wordcloud para visualização no banner

---
+++ TrabalhoFinal/codigos/wordCloudMask.py
@@ -0,0 +1,39 @@
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(sys.argv[1]).read()
+
+# read the mask / color image taken from
+# http://jirkavinse.deviantart.com/art/quot-Real-Life-quot-Alice-282261010
+alice_coloring = np.array(Image.open(sys.argv[2]))
+#stopwords = set(STOPWORDS)
+#stopwords.add("said")
+
+wc = WordCloud(background_color="white", max_words=2000, mask=alice_coloring,
+               max_font_size=40, random_state=42)
+# generate word cloud
+wc.generate(text)
+
+# create coloring from image
+image_colors = ImageColorGenerator(alice_coloring)
+
+# show
+plt.imshow(wc, interpolation="bilinear")
+plt.axis("off")
+plt.figure()
+# recolor wordcloud and show
+# we could also give color_func=image_colors directly in the constructor
+plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+plt.axis("off")
+plt.figure()
+plt.imshow(alice_coloring, cmap=plt.cm.gray, interpolation="bilinear")
+plt.axis("off")
+plt.show()
